2e851b743795e6e4aa5e26e263385be1069bb2d4
amymaryflores <amyflores125@gmail.com>
2025-01-16 10:23:20 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,6 @@ const todosList = document.querySelector(".todos__list");
 const todoTemplateSelector = "#todo-template";
 const todoCounter = new TodoCounter(initialTodos, ".counter__text");
 
-const renderer = (item) => {
-    const todo = new Todo(item, todoTemplateSelector, handleCheck, handleDelete);
-    return todo.getView();
-  };
-
 const generateTodo = (data) => {
   const todo = new Todo(data, todoTemplateSelector, handleCheck, handleDelete);
   return todo.getView();
@@ -76,7 +71,6 @@ const newFormValidator = new FormValidator(validationConfig, addTodoForm);
 newFormValidator.enableValidation();
 
 addTodoPopup.resetForm = () => {
-  addTodoForm.reset();
   newFormValidator.resetValidation();
 };
 
